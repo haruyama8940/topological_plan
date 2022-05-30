ea7d2bd45a65dff26347d0ce86d0c45ae98df63b
haruyama8940 <yaruo0406@gmail.com>
2022-05-30 19:29:13 +0900

    fix pub and srv

--- scripts/moving_distance.py
+++ scripts/moving_distance.py
@@ -1,58 +1,64 @@
 #!/usr/bin/env python3 
-from pygments import lex
 import rospy 
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Float32
+from std_srvs.srv import SetBool,SetBoolRequest,SetBoolResponse
 import numpy as np
-import tf
-from tf.transformations import euler_from_quaternion
 
-odom_x, odom_y = 0.0, 0.0
-flag =0
-odom_x_list = np.array([],dtype=float)
-odom_y_list = np.array([],dtype=float)
+class distance_diff():
+    def __init__(self):
+        
+        self.odom_x, self.odom_y,self.distance_m = 0.0, 0.0, 0.0
+        self.flag =0
+        self.branch_flag = False
+        self.odom_x_list = np.array([],dtype=float)
+        self.odom_y_list = np.array([],dtype=float)
+        self.odom_subscriber = rospy.Subscriber('/camera/odom/sample', Odometry, self.callback_odom)
+        self.distance_pub = rospy.Publisher('moving_distance',Float32,queue_size=1)
+        # self.distance_set = rospy.Service('distance_m',SetBool,callback_branch)
+        #self.resp,self.req
 
-def distance(x_list, y_list):
-    # A = np.array([x_list,np.ones(len(x_list))])
-    # A = A.T
-    # a,b = np.linalg.lstsq(A,y_list)[0]
-    # y0 = a*x_list[0] + b
-    # y_last = a*x_list[-1] + b
-    # odom_pose_0 = np.append(x_list[0],y0)
-    # odom_pose_last = np.append(x_list[-1],y_last)
-    odom_pose_0 =np.array([x_list[0],y_list[0]])
-    odom_pose_last = np.array([x_list[-1],y_list[-1]])
-    long = np.linalg.norm(odom_pose_0-odom_pose_last)
-    return long
+    def callback_odom(self,msg):
+        odom_x = msg.pose.pose.position.x  
+        odom_y = msg.pose.pose.position.y
+        self.flag +=1
+        if self.flag == 50:
+            self.odom_x_list =np.append(self.odom_x_list,odom_x )
+            self.odom_y_list =np.append(self.odom_y_list,odom_y)
+            self.pub_long =self.distance(self.odom_x_list,self.odom_y_list)
+            self.distance_pub.publish(self.pub_long)
+            self.flag=0
 
-def callback_odom(msg):
-    global odom_x, odom_y, odom_x_list,odom_y_list,flag
-    odom_x = msg.pose.pose.position.x  
-    odom_y = msg.pose.pose.position.y
-    flag +=1
-    # print(flag)
-    if flag == 50:
-        odom_x_list =np.append(odom_x_list,odom_x )
-        odom_y_list =np.append(odom_y_list,odom_y)
-        flag=0
-        pub_long =distance(odom_x_list,odom_y_list)
-        print(pub_long ,"[m]")
-    # rospy.loginfo("Odomery: x=%s y=%s", odom_x_list, odom_y_list,)
-    # rospy.loginfo("x_shape = %s, y_shape =%s" ,len(odom_x_list),len(odom_y_list))
+    def distance(self,x_list, y_list):
+        A = np.vstack([x_list, np.ones(len(x_list))]).T
+        a,b = np.linalg.lstsq(A,y_list,rcond=None)[0]
+        y0 = a*x_list[0] + b
+        y_last = a*x_list[-1] + b
+        odom_pose_0 = np.append(x_list[0],y0)
+        odom_pose_last = np.append(x_list[-1],y_last)
+        odom_pose_0 =np.array([x_list[0],y_list[0]])
+        odom_pose_last = np.array([x_list[-1],y_list[-1]])
+        long = np.linalg.norm(odom_pose_0-odom_pose_last)
+        return long
 
 
-# def callback_branch(msg):
-#     if msg.data ==True:
-#         odom_x_list = np.zeros
-#         odom_y_list = np.zeros
-
-def odometry():
-    rospy.init_node('odometry')
-    odom_subscriber = rospy.Subscriber('/camera/odom/sample', Odometry, callback_odom)
-    rospy.loginfo("start")
-    rospy.spin()
-
+    def callback_branch(self,data):
+        resp = SetBoolResponse()
+        req = SetBoolRequest()
+        if data.data ==True:
+            self.odom_x_list = np.zeros
+            self.odom_y_list = np.zeros
+            self.branch_flag = True
+            self.distance_m = data.data
+            resp.message = "distance set"
+            
 if __name__ == '__main__':
-    odometry()
+    rospy.init_node('moving_distance',anonymous=True)
+    diff = distance_diff()
+
+    while not rospy.is_shutdown():
+        rospy.sleep(0.1)
+
 
 
 
